ecfe81bf0b79924b865f79774f4925649443bf03
HeeYong Lee <gmldyd031@gmail.com>
2023-03-22 15:08:48 +0900

    project start1

--- 00_SemiProject/src/com/hotel/model/HotelDAO.java
+++ 00_SemiProject/src/com/hotel/model/HotelDAO.java
@@ -1,5 +1,53 @@
 package com.hotel.model;
 
-public class HotelDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class HotelDAO {
+	private static HotelDAO instance;
+	
+	private Connection con = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+	private String sql;
+	
+	public HotelDAO() {	}
+	
+	public static HotelDAO getInstance() {
+		if(instance == null) {
+			instance = new HotelDAO();
+		}
+		instance.connect();
+		return instance;
+	}
+	
+	private void connect() {
+		try {
+			Context initCtx = new InitialContext();
+			Context ctx = (Context)initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource)ctx.lookup("jdbc/myoracle");
+			con = ds.getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void close() {
+		try {
+			if(rs!=null) rs.close();
+			if(ps!=null) ps.close();
+			if(con!=null) con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
--- 00_SemiProject/src/com/hotel/model/HotelDTO.java
+++ 00_SemiProject/src/com/hotel/model/HotelDTO.java
@@ -1,5 +1,78 @@
 package com.hotel.model;
 
 public class HotelDTO {
+	private int HOTELID;
+	private String HOTELNAME;
+	private String HOTELPHONE;
+	private int HOTELSTAR;
+	private int HOTELPOINT;
+	private String WIFI;
+	private String RESTAURANT;
+	private String PARKING;
+	
+	public HotelDTO() {	}
 
+	public int getHOTELID() {
+		return HOTELID;
+	}
+
+	public void setHOTELID(int hOTELID) {
+		HOTELID = hOTELID;
+	}
+
+	public String getHOTELNAME() {
+		return HOTELNAME;
+	}
+
+	public void setHOTELNAME(String hOTELNAME) {
+		HOTELNAME = hOTELNAME;
+	}
+
+	public String getHOTELPHONE() {
+		return HOTELPHONE;
+	}
+
+	public void setHOTELPHONE(String hOTELPHONE) {
+		HOTELPHONE = hOTELPHONE;
+	}
+
+	public int getHOTELSTAR() {
+		return HOTELSTAR;
+	}
+
+	public void setHOTELSTAR(int hOTELSTAR) {
+		HOTELSTAR = hOTELSTAR;
+	}
+
+	public int getHOTELPOINT() {
+		return HOTELPOINT;
+	}
+
+	public void setHOTELPOINT(int hOTELPOINT) {
+		HOTELPOINT = hOTELPOINT;
+	}
+
+	public String getWIFI() {
+		return WIFI;
+	}
+
+	public void setWIFI(String wIFI) {
+		WIFI = wIFI;
+	}
+
+	public String getRESTAURANT() {
+		return RESTAURANT;
+	}
+
+	public void setRESTAURANT(String rESTAURANT) {
+		RESTAURANT = rESTAURANT;
+	}
+
+	public String getPARKING() {
+		return PARKING;
+	}
+
+	public void setPARKING(String pARKING) {
+		PARKING = pARKING;
+	}
 }
