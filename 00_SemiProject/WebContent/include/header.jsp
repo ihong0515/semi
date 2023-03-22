cd12decb554091797072ecfde2952ed67d7c1a0d
HongJaeYoung <ghddl9805@gmail.com>
2023-03-22 17:51:02 +0900

    hong

---
+++ 00_SemiProject/WebContent/include/header.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+		
+		<nav><!--상단 우측 로그인 회원가입, 비밀번호 찾기  -->
+          <ul>
+            <li><a href="">로그인</a></li>
+            <li><a href="">비밀번호찾기</a></li>
+            <li><a href="">회원가입</a></li>
+            <li><a href="">고객센터</a></li>
+            
+          </ul>
+        </nav>
+		
+		
+		<nav><!--구현 기능접속 네비바  -->
+          <ul>
+            <li><a href="">호텔검색</a></li>
+            <li><a href="">업체등록페이지</a></li>
+            <li><a href="">마이페이지</a></li>
+            <li><a href="">고객센터</a></li>
+            
+          </ul>
+        </nav>
+</body>
+</html>
--- 00_SemiProject/src/com/hotel/action/HotelListAction.java
+++ 00_SemiProject/src/com/hotel/action/HotelListAction.java
@@ -1,6 +1,7 @@
 package com.hotel.action;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,16 @@ public class HotelListAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String hotel_location = request.getParameter("location");
+		String hotel_checkinDate = request.getParameter("checkinDate");
+		String hotel_checkoutDate = request.getParameter("checkoutDate");
+		String hotel_keyword = request.getParameter("keyword");
+	
+		PrintWriter out = response.getWriter();
 		
+		out.println(hotel_location);
+		out.println(hotel_checkinDate);
+		out.println(hotel_checkoutDate);
+		out.println(hotel_keyword);
 	}
 }
