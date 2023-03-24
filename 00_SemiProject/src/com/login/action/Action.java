353f98d747e33876f0a0e6216a0a513de60edba7
jis555 <jisoo5429@naver.com>
2023-03-24 14:44:05 +0900

    20230324 choijisoo

--- 00_SemiProject/src/com/login/action/Action.java
+++ 00_SemiProject/src/com/login/action/Action.java
@@ -1,5 +1,10 @@
 package com.login.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface Action {
+	
+	public void execute(HttpServletRequest request, HttpServletResponse response);
 
 }
