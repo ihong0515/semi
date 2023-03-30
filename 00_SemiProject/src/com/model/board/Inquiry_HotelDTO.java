5bc96772baaa993a36ef3290ed9589254f7347ce
HongJaeYoung <ghddl9805@gmail.com>
2023-03-30 09:06:11 +0900

    morning

---
+++ 00_SemiProject/src/com/model/board/Inquiry_HotelDTO.java
@@ -0,0 +1,94 @@
+package com.model.board;
+
+public class Inquiry_HotelDTO {
+	private int inqho_no;
+	private int inqho_ownerno;
+	private int inqho_hotelno;
+	private String inqho_writer;
+	private String inqho_title;
+	private String inqho_content;
+	private String inqho_date;
+	private String inqho_update;
+	private int inqho_group;
+	private int inqho_step;
+	private int inqho_indent;
+	
+	
+	
+	public int getInqho_no() {
+		return inqho_no;
+	}
+	public void setInqho_no(int inqho_no) {
+		this.inqho_no = inqho_no;
+	}
+	public int getInqho_ownerno() {
+		return inqho_ownerno;
+	}
+	public void setInqho_ownerno(int inqho_ownerno) {
+		this.inqho_ownerno = inqho_ownerno;
+	}
+	public int getInqho_hotelno() {
+		return inqho_hotelno;
+	}
+	public void setInqho_hotelno(int inqho_hotelno) {
+		this.inqho_hotelno = inqho_hotelno;
+	}
+	public String getInqho_writer() {
+		return inqho_writer;
+	}
+	public void setInqho_writer(String inqho_writer) {
+		this.inqho_writer = inqho_writer;
+	}
+	public String getInqho_title() {
+		return inqho_title;
+	}
+	public void setInqho_title(String inqho_title) {
+		this.inqho_title = inqho_title;
+	}
+	public String getInqho_content() {
+		return inqho_content;
+	}
+	public void setInqho_content(String inqho_content) {
+		this.inqho_content = inqho_content;
+	}
+	public String getInqho_date() {
+		return inqho_date;
+	}
+	public void setInqho_date(String inqho_date) {
+		this.inqho_date = inqho_date;
+	}
+	public String getInqho_update() {
+		return inqho_update;
+	}
+	public void setInqho_update(String inqho_update) {
+		this.inqho_update = inqho_update;
+	}
+	public int getInqho_group() {
+		return inqho_group;
+	}
+	public void setInqho_group(int inqho_group) {
+		this.inqho_group = inqho_group;
+	}
+	public int getInqho_step() {
+		return inqho_step;
+	}
+	public void setInqho_step(int inqho_step) {
+		this.inqho_step = inqho_step;
+	}
+	public int getInqho_indent() {
+		return inqho_indent;
+	}
+	public void setInqho_indent(int inqho_indent) {
+		this.inqho_indent = inqho_indent;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
